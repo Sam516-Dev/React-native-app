8699a5b278df582eddc7a7c42c74c8eb51a5cf2a
Samuel Njoroge <samuelnjoroge3455@gmail.com>
2025-02-01 12:04:54 +0300

    Initial commit

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const index = () => {
+  return (
+    <View>
+      
+      <Link href="/sign-in">SignIn</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Properties</Link>
+
+    </View>
+  )
+}
+
+export default index
