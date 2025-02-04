b93ea30837918379a47c89a02d97c574475cc9e2
Samuel Njoroge <samuelnjoroge3455@gmail.com>
2025-02-04 16:35:06 +0300

    Add Tailwind CSS integration and initial assets setup

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,18 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import { View, Text } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 
 const index = () => {
   return (
     <View>
-      
+      <Text className="font-bold text-red-700 text-lg">
+        This is my heading test !
+      </Text>
       <Link href="/sign-in">SignIn</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
       <Link href="/properties/1">Properties</Link>
-
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default index;
