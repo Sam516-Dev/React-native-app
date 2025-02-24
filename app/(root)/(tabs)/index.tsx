eb11d1ac7ef89baa9d41d4e56329b47b7ed97826
Samuel Njoroge <samuelnjoroge3455@gmail.com>
2025-02-24 10:40:12 +0300

    Refactor sign-in screen layout and remove unnecessary heading

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -5,9 +5,6 @@ import { Link } from "expo-router";
 const index = () => {
   return (
     <View>
-      <Text className=" text-red-700 shadow-slate-900 drop-shadow-md  text-center py-8 bg-blue-300 font-rubik-bold text-3xl">
-        This is my heading test !
-      </Text>
       <Link href="/sign-in">SignIn</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, TextInput, TouchableOpacity, } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import images from "@/constants/images";
 
 const signin = () => {
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6 object-contain "
+        />
 
-export default signin
+        <View>
+          <Text className="text-2xl font-bold text-center mt-8">
+            Welcome to Samuel
+          </Text>
+          <Text className="text-center mt-4">
+            Sign in to continue to Samuel
+          </Text>
+        </View>
+
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default signin;
