f59f4696e79f178c348cfe1a11100952cc674725
Samuel Njoroge <samuelnjoroge3455@gmail.com>
2025-03-31 22:50:38 +0300

    check in

---
+++ components/homeMade.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const homeMade = () => {
+  return (
+    <View>
+      <Text>homeMade</Text>
+    </View>
+  )
+}
+
+export default homeMade
